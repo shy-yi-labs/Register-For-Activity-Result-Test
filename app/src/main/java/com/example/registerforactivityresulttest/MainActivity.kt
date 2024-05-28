b36ceabb3d9622c973fe0ec424f3b44487953ab3
shy.yi.labs <shy.yi.labs@gmail.com>
2024-05-28 20:59:43 +0900

    1

--- app/src/main/java/com/example/registerforactivityresulttest/MainActivity.kt
+++ app/src/main/java/com/example/registerforactivityresulttest/MainActivity.kt
@@ -5,12 +5,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +35,44 @@ class MainActivity : AppCompatActivity() {
         launcher.launch(
             arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
         )
+    }
+}
+
+fun <I, O> ComponentActivity.registerForActivityResultDelegate(
+    contract: ActivityResultContract<I, O>,
+    callback: ActivityResultCallback<O>
+): ActivityResultLauncher<I> {
+    return ActivityResultRegisterDelegate(
+        context = this,
+        shouldShowRequestPermissionRationale = this::shouldShowRequestPermissionRationale,
+        contract = contract,
+        callback = callback,
+        registerActivityForResult = this::registerForActivityResult
+    ).register()
+}
+
+fun <I, O> Fragment.registerForActivityResultDelegate(
+    contract: ActivityResultContract<I, O>,
+    callback: ActivityResultCallback<O>
+): ActivityResultLauncher<I> {
+    return ActivityResultRegisterDelegate(
+        context = requireContext(),
+        shouldShowRequestPermissionRationale = this::shouldShowRequestPermissionRationale,
+        contract = contract,
+        callback = callback,
+        registerActivityForResult = this::registerForActivityResult
+    ).register()
+}
+
+class ActivityResultRegisterDelegate<I, O>(
+    private val context: Context,
+    private val shouldShowRequestPermissionRationale: (String) -> Boolean,
+    private val contract: ActivityResultContract<I, O>,
+    private val callback: ActivityResultCallback<O>,
+    private val registerActivityForResult: (ActivityResultContract<I, O>, ActivityResultCallback<O>) -> ActivityResultLauncher<I>
+) {
+
+    fun register(): ActivityResultLauncher<I> {
+        return registerActivityForResult(contract, callback)
     }
 }
