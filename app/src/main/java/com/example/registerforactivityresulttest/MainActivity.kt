22f2be6aa68996e6a0fd1c086e658881ad6d494e
shy.yi.labs <shy.yi.labs@gmail.com>
2024-05-28 20:40:33 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/registerforactivityresulttest/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.registerforactivityresulttest
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+        Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        launcher.launch(
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        )
+    }
+}
