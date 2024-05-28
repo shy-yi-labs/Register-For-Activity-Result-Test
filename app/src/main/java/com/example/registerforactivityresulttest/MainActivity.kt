31cd8feb876e3bf435df1c2a210868b8e8e74ba4
shy.yi.labs <shy.yi.labs@gmail.com>
2024-05-28 22:56:18 +0900

    RationaleActivityResultLauncher 및 ConditionalMultiplePermissionRequestContract 구현

--- app/src/main/java/com/example/registerforactivityresulttest/MainActivity.kt
+++ app/src/main/java/com/example/registerforactivityresulttest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.registerforactivityresulttest
 import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -11,15 +12,41 @@ import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+class HomeFragment : Fragment() {
+    private val launcher = registerForActivityResultWithRationale(
+        rationaleMessage = "권한이 필요해요.",
+        onNegativeButtonClick = {
+            Toast.makeText(requireContext(), "권한 거부", Toast.LENGTH_SHORT).show()
+        },
+        contract = ConditionalMultiplePermissionRequestContract(
+            resultPredicate = { permissionStateMap -> permissionStateMap.values.all { it } },
+            requestPredicate = { permissionStateMap -> permissionStateMap.values.all { it }.not() }
+        )
+    ) {
+
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
-    private val launcher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-        Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
+    private val launcher = registerForActivityResultWithRationale(
+        rationaleMessage = "권한이 필요해요.",
+        onNegativeButtonClick = {
+            Toast.makeText(this, "권한 거부", Toast.LENGTH_SHORT).show()
+        },
+        contract = ConditionalMultiplePermissionRequestContract(
+            resultPredicate = { permissionStateMap -> permissionStateMap.values.all { it } },
+            requestPredicate = { permissionStateMap -> permissionStateMap.values.all { it }.not() }
+        )
+    ) {
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,41 +65,115 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-fun <I, O> ComponentActivity.registerForActivityResultDelegate(
-    contract: ActivityResultContract<I, O>,
+fun <O> ComponentActivity.registerForActivityResultWithRationale(
+    rationaleMessage: String,
+    onNegativeButtonClick: () -> Unit,
+    contract: ActivityResultContract<Array<String>, O>,
     callback: ActivityResultCallback<O>
-): ActivityResultLauncher<I> {
-    return ActivityResultRegisterDelegate(
+): ActivityResultLauncher<Array<String>> {
+    return registerForActivityResultWithRationale(
         context = this,
         shouldShowRequestPermissionRationale = this::shouldShowRequestPermissionRationale,
+        rationaleMessage = rationaleMessage,
+        onNegativeButtonClick = onNegativeButtonClick,
         contract = contract,
         callback = callback,
         registerActivityForResult = this::registerForActivityResult
-    ).register()
+    )
 }
 
-fun <I, O> Fragment.registerForActivityResultDelegate(
-    contract: ActivityResultContract<I, O>,
+fun <O> Fragment.registerForActivityResultWithRationale(
+    rationaleMessage: String,
+    onNegativeButtonClick: () -> Unit,
+    contract: ActivityResultContract<Array<String>, O>,
     callback: ActivityResultCallback<O>
-): ActivityResultLauncher<I> {
-    return ActivityResultRegisterDelegate(
+): ActivityResultLauncher<Array<String>> {
+    return registerForActivityResultWithRationale(
         context = requireContext(),
         shouldShowRequestPermissionRationale = this::shouldShowRequestPermissionRationale,
+        rationaleMessage = rationaleMessage,
+        onNegativeButtonClick = onNegativeButtonClick,
         contract = contract,
         callback = callback,
         registerActivityForResult = this::registerForActivityResult
-    ).register()
+    )
 }
 
-class ActivityResultRegisterDelegate<I, O>(
-    private val context: Context,
-    private val shouldShowRequestPermissionRationale: (String) -> Boolean,
-    private val contract: ActivityResultContract<I, O>,
-    private val callback: ActivityResultCallback<O>,
-    private val registerActivityForResult: (ActivityResultContract<I, O>, ActivityResultCallback<O>) -> ActivityResultLauncher<I>
-) {
+fun <O> registerForActivityResultWithRationale(
+    context: Context,
+    shouldShowRequestPermissionRationale: (String) -> Boolean,
+    rationaleMessage: String,
+    onNegativeButtonClick: () -> Unit,
+    contract: ActivityResultContract<Array<String>, O>,
+    callback: ActivityResultCallback<O>,
+    registerActivityForResult: (ActivityResultContract<Array<String>, O>, ActivityResultCallback<O>) -> ActivityResultLauncher<Array<String>>
+): ActivityResultLauncher<Array<String>> {
+    return RationaleActivityResultLauncher(
+        context = context,
+        shouldShowRequestPermissionRationale = shouldShowRequestPermissionRationale,
+        rationaleMessage = rationaleMessage,
+        onNegativeButtonClick = onNegativeButtonClick,
+        launcher = registerActivityForResult(contract, callback)
+    )
+}
 
-    fun register(): ActivityResultLauncher<I> {
-        return registerActivityForResult(contract, callback)
+class RationaleActivityResultLauncher(
+    private val context: Context,
+    private val launcher: ActivityResultLauncher<Array<String>>,
+    private val rationaleMessage: String,
+    private val shouldShowRequestPermissionRationale: (String) -> Boolean,
+    private val onNegativeButtonClick: () -> Unit
+) : ActivityResultLauncher<Array<String>>() {
+    override val contract: ActivityResultContract<Array<String>, *>
+        get() = launcher.contract
+
+    override fun unregister() {
+        launcher.unregister()
+    }
+
+    override fun launch(input: Array<String>, options: ActivityOptionsCompat?) {
+        val permissionsToShowRationale = input.filter { shouldShowRequestPermissionRationale(it) }
+
+        if (permissionsToShowRationale.isNotEmpty()) {
+            AlertDialog.Builder(context)
+                .setMessage(rationaleMessage)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    launcher.launch(input, options)
+                }
+                .setNegativeButton(android.R.string.cancel) { _, _ ->
+                    onNegativeButtonClick()
+                }
+                .show()
+        } else {
+            launcher.launch(input, options)
+        }
+    }
+}
+
+class ConditionalMultiplePermissionRequestContract(
+    private val resultPredicate: (Map<String, Boolean>) -> Boolean,
+    private val requestPredicate: (Map<String, Boolean>) -> Boolean = resultPredicate
+): ActivityResultContract<Array<String>, Boolean>() {
+
+    private val multiplePermissionRequestContract = ActivityResultContracts.RequestMultiplePermissions()
+    override fun createIntent(context: Context, input: Array<String>): Intent {
+        return multiplePermissionRequestContract.createIntent(context, input)
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): Boolean {
+        return resultPredicate(multiplePermissionRequestContract.parseResult(resultCode, intent))
+    }
+
+    override fun getSynchronousResult(
+        context: Context,
+        input: Array<String>
+    ): SynchronousResult<Boolean>? {
+        val permissionStateMap = input.associateWith {
+            ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+        }
+
+        return if (requestPredicate(permissionStateMap)) {
+            SynchronousResult(true)
+        } else null
     }
 }
